a70149da62eeb164052e67bd84511415c793091c
Ashwin V <asvk2002@gmail.com>
2024-07-12 10:06:14 +0530

    commit

---
+++ script.js
@@ -0,0 +1,9 @@
+let box=document.getElementById('box');
+
+box.addEventListener('mouseover',()=>{box.style.background="black"});
+box.addEventListener('mouseout',()=>{box.style.background="white"});
+
+let box2=document.getElementById('box2');
+
+box2.addEventListener('mouseover',()=>{box2.style.background="black"});
+box2.addEventListener('mouseout',()=>{box2.style.background="white"});
